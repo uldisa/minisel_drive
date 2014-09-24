e09c599b3393b66c27949e23b245862439e43037
Uldis Ansmits <uldis.ansmits@gmail.com>
2014-09-24 22:59:49 +0300

    Inital commit

---
+++ Makefile
@@ -0,0 +1,33 @@
+include Arduino.mk
+
+LIBS=core SPI minisel_lcd
+#PORT=/dev/ttyACM0
+#PORT=COM11
+#CPU=atmega2560
+#BR=115200
+#CPPFLAGS+=-DARDUINO_AVR_MEGA2560 -DARDUINO_ARCH_AVR
+#VARIANT=mega
+#PROGRAMMER=wiring
+#PORT=/dev/ttyUSB0
+PORT=COM10
+CPU=atmega328p
+BR=57600
+CPPFLAGS+=-DARDUINO_AVR_PRO -DARDUINO_ARCH_AVR
+VARIANT=standard
+PROGRAMMER=arduino
+
+USER_LIB_PATH=$(CURDIR)/libraries
+WIRE_LIB_PATH=$(ARD_HOME)/hardware/arduino/avr/libraries/Wire
+include lib.mk
+
+ifeq ($(filter %-pc-cygwin,$(MAKE_HOST)),)
+CPPFLAGS+=-I$(WIRE_LIB_PATH) -I$(WIRE_LIB_PATH)/utility
+else
+CPPFLAGS+=-I$(shell cygpath -m $(WIRE_LIB_PATH)) -I$(shell cygpath -m $(WIRE_LIB_PATH)/utility)
+endif
+
+CPPFLAGS+=-Wall -Wextra -I. -Os -fno-exceptions -ffunction-sections -fdata-sections
+
+main.elf: main.o libcore.a libminisel_lcd.a libSPI.a
+
+
---
+++ main.cpp
@@ -0,0 +1,192 @@
+#include <Arduino.h>
+#include <minisel_lcd.h>
+
+#define ZERO_PIN 2
+#define PWM_PIN 9
+
+MINISEL_LCD lcd;
+
+volatile unsigned int min_HIGH=-1;
+volatile unsigned int min_LOW=-1;
+volatile int counter=0;
+unsigned long lastDebounceTime=0;
+unsigned long debounceDelay=1;
+int lastSelectorValue;
+volatile int phase=0;
+void calibratePeriod(){
+		// Wait for stabilized startup
+		if(counter>5) {
+			if(digitalRead(ZERO_PIN)) {
+				if(min_HIGH>TCNT1) {
+					min_HIGH=TCNT1;
+				}
+			} else {
+				if(min_LOW>TCNT1) {
+					min_LOW=TCNT1;
+				}
+			}
+		}
+		counter++;
+		TCNT1=0;
+}
+void setup() {
+	cli();
+	// Set CPU prescaler to 4Mhz
+	CLKPR = 0b10000000;
+	CLKPR = 0b00000010;
+  	//Set Timer2 for LCD update because lower priority Timer0 is used by millisleep.
+	TCCR2A = 0b00000000;	//Normal Timer2 mode.
+	TCCR2B = 0b00000100;	//Prescale 16Mhz/64
+	TIMSK2 = 0b00000001;	//Enable overflow interrupt
+	// Set Timer1 for calibration loop
+	TCCR1A=0;  //Clear registers
+	TCNT1=0;
+	TIMSK1=0;
+	TCCR1B=0b00000010; //Prescale 16Mhz/8. Maximum counter value without owerflow in 1/2 period
+	pinMode(ZERO_PIN, INPUT);
+	pinMode(PWM_PIN, OUTPUT);
+	digitalWrite(PWM_PIN ,LOW);
+	sei();
+}
+
+// Absolute program switch position reading values. 
+// Less than 16% off calculated position value
+/*int levels[]={
+0,
+112,
+205,
+281,
+344,
+397,
+443,
+483,
+518,
+548,
+576,
+600,
+622,
+641,
+659,
+675,
+692,
+704,
+716,
+730
+};*/
+// the loop routine runs over and over again forever:
+ISR(TIMER1_COMPA_vect){
+	digitalWrite(PWM_PIN, HIGH);
+}
+
+ISR(TIMER1_COMPB_vect){
+	TCCR1B=0b00000000; // Stop timer
+	digitalWrite(PWM_PIN, LOW);
+}
+ISR(TIMER2_OVF_vect)
+{
+	lcd.update();
+}
+
+void phaseStart(){
+	// Wait for stabilized startup
+	TCCR1B=0b00000000; // Stop timer
+	digitalWrite(PWM_PIN, LOW);
+	if(!phase) {
+		return; //Triac off
+	}
+	int max_value;
+	if(digitalRead(ZERO_PIN)) {
+		max_value=min_HIGH;
+	} else {
+		max_value=min_LOW;
+	}
+	int pulseLen=max_value/50;
+	max_value=max_value-pulseLen;
+	int pulseStart=max_value/19*(19-phase);
+	if(pulseStart>max_value-pulseLen){
+		pulseStart=max_value-pulseLen-pulseLen/2;
+	}
+	int pulseEnd=pulseStart+pulseLen;
+	OCR1A=pulseStart;
+	OCR1B=pulseEnd;
+	TCNT1=-1;
+	TCCR1B=0b00000010; //Prescale 16Mhz/8. Maximum counter value without owerflow in 1/2 period
+
+}
+uint8_t numbers_19[]={
+Ms0,
+Ms1,
+Ms2,
+Ms3,
+Ms4,
+Ms5,
+Ms6,
+Ms7,
+Ms8,
+Ms9,
+MsDOT|Ms0,
+MsDOT|Ms1,
+MsDOT|Ms2,
+MsDOT|Ms3,
+MsDOT|Ms4,
+MsDOT|Ms5,
+MsDOT|Ms6,
+MsDOT|Ms7,
+MsDOT|Ms8,
+MsDOT|Ms9
+};
+
+int main(void) {
+	init();
+	setup();
+	lcd.on(MsBACKLIGHT);
+	counter=0;
+	lcd.on(MsTIMER|numbers_19[0]);
+	attachInterrupt(0, calibratePeriod, CHANGE);	
+	lcd.on(MsTIMER|numbers_19[1]);
+	while(counter<25){}
+	lcd.on(MsTIMER|numbers_19[2]);
+	TCCR1B=0b00000000; // Stop timer
+	lcd.on(MsTIMER|numbers_19[3]);
+	detachInterrupt(0);	
+	lcd.on(MsTIMER|numbers_19[4]);
+	TIMSK1=0b00000110; // Interrupt un OCR1A nad OCR1B match
+	lcd.on(MsTIMER|numbers_19[5]);
+	phase=0;
+	lcd.on(MsTIMER|numbers_19[6]);
+	attachInterrupt(0, phaseStart, CHANGE);	
+	lcd.on(MsTIMER|numbers_19[7]);
+	while(1) {
+		loop();
+	}		
+
+}	
+
+//#define MAX_VAL 1023
+//#define R18_VAL	25.5
+//#define POT_VAL 63
+
+void loop() {
+  // read the input on analog pin 0:
+  int selectorValue = analogRead(A0);
+
+  if(abs(lastSelectorValue-selectorValue)>4) {
+     lastDebounceTime=millis();
+  	lastSelectorValue=selectorValue;
+  } 
+ 	
+  if((millis()-lastDebounceTime)>debounceDelay){
+	phase=round((double)(25.5*selectorValue*19.0)/((double)(1023-selectorValue)*63));
+	// Print selector position
+	lcd.on(MsTIMER|numbers_19[phase]);
+	// Print selectorValue
+  	int p=selectorValue;
+	lcd.on(MsHOUR|numbers_19[p/100]);
+	p-=((int)p/100)*100;
+	lcd.on(MsMINU10|numbers_19[p/10]);
+	p-=((int)p/10)*10;
+	lcd.on(MsMINUTE|numbers_19[p]);
+     lastDebounceTime=millis();
+
+  }
+}
